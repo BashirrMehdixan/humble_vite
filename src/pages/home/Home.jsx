20efb0a579ad6f8cd3d50665eb32e7bac05c29d8
Bashirr <beshirr096@gmail.com>
2024-04-03 17:24:51 +0400

    swiper js added

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,9 +1,11 @@
 import HomeBanner from "/src/pages/home/HomeBanner";
+import Featured from "/src/pages/home/Featured";
 
 const Home = () => {
     return (
         <>
             <HomeBanner/>
+            <Featured/>
         </>
     )
 }
