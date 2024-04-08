bbdf37548dc16c367bd58dc1b1132455185948e5
Bashirr <beshirr096@gmail.com>
2024-04-08 16:52:20 +0400

    genres added and some changes

--- src/pages/games/GameDetail.jsx
+++ src/pages/games/GameDetail.jsx
@@ -5,7 +5,7 @@ import {GameContext} from "/src/context/Games/GameContext";
 import {Swiper, SwiperSlide} from 'swiper/react';
 import {FreeMode, Navigation, Thumbs} from 'swiper/modules';
 
-// Import Swiper styles
+
 import 'swiper/css';
 import 'swiper/css/free-mode';
 import 'swiper/css/navigation';
@@ -27,6 +27,9 @@ const GameDetail = () => {
                             {game.name}
                         </h3>
                     </div>
+                    <ul className="detail-list">
+                        {game.genres.map((genre, index) => <li key={index}>{genre.name}</li>)}
+                    </ul>
                     <Swiper
                         loop={true}
                         spaceBetween={10}
@@ -61,6 +64,15 @@ const GameDetail = () => {
                             )
                         })}
                     </Swiper>
+                </div>
+
+                <div className="game-description">
+                    The most awarded game of a generation, now enhanced for the next! Experience The Witcher 3: Wild
+                    Hunt and its expansions in this definitive collection, boasting improved visuals and performance,
+                    new additional content, Photo Mode, and more!
+                    <ul className="detail-list">
+                        {game.genres.map((genre, index) => <li key={index}>{genre.name}</li>)}
+                    </ul>
                 </div>
             </div>
         </>
